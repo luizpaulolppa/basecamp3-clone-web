b91bc9bba59b543453f7aed7c98e41554dc1a0cb
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-02-14 19:07:48 -0300

    fix server js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/build"));
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/build/index.html');
